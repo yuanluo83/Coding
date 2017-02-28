838de9df1ba6baa2c97a8f4c20b0cc1991c1ca7c
Yuan Luo <yuanluo83@gmail.com>
2017-02-27 22:16:25 -0800

    two more

--- leetcode/algorithms/Trie.java
+++ leetcode/algorithms/Trie.java
@@ -1,7 +1,7 @@
 /*
  * 208. Implement Trie (Prefix Tree)
  * Implement a trie with insert, search, and startsWith methods.
- * Note: You may assume that all inputs are consist of lowercase letters a-z.
+ * Note: You may assume that all inputs are consist of lowercase letters a-z. 
  */
 
 package algorithms; 
